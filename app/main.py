537a0de5a8756f16ecc65ac6d712a884f37a3a0f
blueCormorant <lucavdelsignore@gmail.com>
2022-12-31 14:41:06 -0500

    Fix bug

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_connection, _ = server_socket.accept() # wait for client
     client_connection.recv(1024)
-    client_connection.send(b"+PONG/r/n")
+    client_connection.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
