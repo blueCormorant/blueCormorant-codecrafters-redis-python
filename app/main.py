e46b99f72589e2cae37f4480537c905d3cd1d3a1
blueCormorant <lucavdelsignore@gmail.com>
2022-12-31 16:33:14 -0500

    Stage 4

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 # Uncomment this to pass the first stage
+import threading
 import socket
 
+
+def handle_connection(client_connection):
+    while True:
+        try:
+            client_connection.recv(1024)
+            client_connection.send(b"+PONG\r\n")
+        except ConnectionError:
+            break # Exit thread if the connection closes
+            
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,11 +18,9 @@ def main():
 
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_connection, _ = server_socket.accept() # wait for client
-    
     while True:
-        client_connection.recv(1024)
-        client_connection.send(b"+PONG\r\n")
+        client_connection, _ = server_socket.accept() # wait for client
+        threading.Thread(target=handle_connection, args=(client_connection,)).start()
 
 
 if __name__ == "__main__":
